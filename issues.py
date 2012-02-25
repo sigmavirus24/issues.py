cb13fcd5a4f0871bb695abdb8dbcd3dbb55b1e66
SigmaVirus24 <graffatcolmingov@gmail.com>
2012-02-25 16:17:29 -0500

    Major changes to issues.py. Add .gitignore.
    I forgot that declaring certain things without 'self.' infront, makes them
    global to all instances of that class. So I changed all of those lines and moved
    them inside of the __init__ calls and decided to initialize every variable that
    could be referenced without being properly declared prior to being requested.
    
    Some lines were repetetive and I had created functions to do them after the fact
    on a class, so I replaced the repetetive lines with the function calls.
    
    I added get_headers() and set_url() to GitHub. The latter allows someone to set
    up a GitHub() instance, set the url and then request later instead of being
    forced to set the url when they request and wasting time waiting for the
    request() call to finish.
    
    I added the class 'IssuesParser' which takes in the json data, stores it as a
    string and uses the json module to parse it into python data structures.
    
    I also added the class 'Issues' which provies an easy way to interface with the
    IssuesParser and GitHub classes. Issues lets you provide whatever you'd like,
    owner, project, user, password, oauth_token and provides caching. With this, one
    could do:
    
    example = Issues(owner='sigmavirus24', project='Todo.txt-python')
    example.fetch_issues()
    example.print_issues()
    example.cache()  # or example.cache('.')
    .
    .
    .
    example2 = Issues(owner='sigmavirus24', project='Todo.txt-python')
    example2.fetch_issues(check_cache=True, cache_dir='.')
    example2.print_issues()
    
    And the issues that were stored locally would be used.

--- issues.py
+++ issues.py
@@ -10,15 +10,19 @@ import json
 from base64 import b64encode
 
 class GitHub(object):
-    req = None
-    auth = None
+    """Basic API Interface."""
 
     def __init__(self, user=None, pw=None, oauth_token=None):
+        self.req = None
+        self.auth = None
+        self.last_url = None
+        self.headers = None
+        self.code = None
+        self.data = None
         if user and pw:
-            b64 = b64encode(':'.join([user, pw]))
-            self.auth = ' '.join(['Basic', b64])
+            self.add_basic_auth(user, pw)
         elif oauth_token:
-            self.auth = ' '.join(['token', oauth_token])
+            self.add_oauth(oauth_token)
 
 
     def __request__(self):
@@ -34,6 +38,16 @@ class GitHub(object):
         self.code = response.getcode()
         self.data = response.read()
 
+
+    def add_basic_auth(self, user, pw):
+        """Adds basic authentication to the object."""
+        self.auth = ' '.join(['Basic', b64encode(':'.join([user, pw]))])
+
+
+    def add_oauth(self, oauth_token):
+        """Adds oauth authentication to the object."""
+        self.auth = ' '.join(['token', oauth_token])
+
     
     def get_code(self):
         """Return the HTTP code for the last request."""
@@ -45,15 +59,143 @@ class GitHub(object):
         return self.data
 
 
+    def get_headers(self):
+        """Return the stored headers object."""
+        return self.headers
+
+
     def get_url(self):
         """Return the stored url used by the last request."""
         return self.last_url
 
 
     def request(self, url=None):
+        """Send a GET request to the GitHub API."""
         if url and url.startswith('https://api.github.com/'):
             self.last_url = url
         elif not (self.last_url or url):
             print('All requests must be made to "https://api.github.com/"')
             return
         self.__request__()
+
+
+    def set_url(self, url):
+        """Set url to use for requests."""
+        self.last_url = url
+
+
+class IssuesParser(object):
+    """Handles the parsing of issues"""
+
+    def __init__(self, data=None):
+        self.flat_data = None  # The string returned by the call to the API
+        self.structured_data = None  # The data structure returned by json.loads()
+        self.issues_dict = {}  # Dictionary to keep the issues in
+        self.issue_numbers = []  # Sorted list of issue numbers
+
+        if data:
+            self.parse(data)
+
+
+    def __parse__(self, data):
+        self.flat_data = data.decode()
+        self.structured_data = json.loads(self.flat_data)
+        for d in self.structured_data:
+            self.issues_dict[d['number']] = d
+            self.issue_numbers.append(d['number'])
+            self.issue_numbers.sort()
+
+
+    def get_flat_data(self):
+        return self.flat_data
+
+
+    def parse(self, data):
+        if isinstance(data, str):
+            self.__parse__(data)
+        elif isinstance(data, GitHub):
+            self.__parse__(data.get_data())
+
+
+    def print_issues(self):
+        """Print the issues that were just parsed."""
+        format_str = '{num:>3} {title} ({author}) {extra}'
+        if self.issues_dict and self.issue_numbers:
+            for n in self.issue_numbers:
+                title = self.issues_dict[n]['title']
+                author = self.issues_dict[n]['user']['login']
+                assignee = None
+                if self.issues_dict[n]['assignee']:
+                    assignee = self.issues_dict[n]['assignee']['login']
+                labels = '@{0} '.format(assignee) if assignee else '' 
+                for l in self.issues_dict[n]['labels']:
+                    labels = ''.join([labels, '+', l['name'], ' '])
+                print(format_str.format(num=n, title=title, author=author,
+                    extra=labels))
+
+
+class Issues(object):
+    """Provides an easy interface to the Issues list."""
+
+    def __init__(self, owner=None, project=None, user=None, pw=None,
+            oauth_token=None):
+        self.github = GitHub()
+        self.issues = IssuesParser()
+        self.owner = None
+        self.project = None
+        self.url = None
+        self.my_issues = None
+
+        if owner and project:
+            self.owner = owner
+            self.project = project
+            self.url = 'https://api.github.com/repos/{owner}/{proj}/issues'
+            self.url = self.url.format(owner=self.owner, proj=self.project)
+            self.github.set_url(self.url)
+        if user:
+            self.user = user
+            self.my_issues = 'https://api.github.com/issues'
+            if pw:
+                self.github.add_basic_auth(user, pw)
+            if not self.github.get_url():
+                self.github.set_url(self.my_issues)
+        if oauth_token:
+            self.github.add_oauth(oauth_token)
+
+
+    def cache(self):
+        if self.owner and self.project:
+            filename = '{0}-{1}.json'.format(self.owner, self.project)
+        elif self.user:
+            filename = '{0}.json'.format(self.user)
+        with open(filename, 'w+') as fd:
+            fd.write(self.issues.get_flat_data())
+
+
+    def fetch_issues(self, owner=None, project=None, check_cache=False,
+            cache_dir=None):
+        if check_cache:
+            from os import listdir
+            owner = owner if owner else self.owner
+            project = project if project else self.project
+            name = '{0}-{1}.json'.format(owner, project)
+            for f in listdir(cache_dir):
+                if f == name:
+                    self.open_cache(f)
+                    return
+
+        url = None
+        if owner and project:
+            url = 'https://api.github.com/repos/{owner}/{proj}/issues'
+            url.format(owner=owner, proj=project)
+        self.github.request(url)
+        self.issues.parse(self.github)
+
+
+    def print_issues(self):
+        self.issues.print_issues()
+
+
+    def open_cache(self, filename):
+        with open(filename, 'r') as fd:
+            self.issues.parse(fd.read())
